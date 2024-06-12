506d9bda627b66a62ccaf400159d9414b9f35cca
Iamouma <stephenouma452@gmail.com>
2024-06-12 13:11:39 +0300

    Updated the javascript files

--- public/script.js
+++ public/script.js
@@ -5,24 +5,33 @@ function toggleMenu() {
     icon.classList.toggle("open");
 }
 
-document.getElementById("contactForm").addEventListener("submit", function(event) {
-    event.preventDefault(); // Prevent default form submission
-
-    // Collect form data
-    const formData = new FormData(this);
-
-    // Send form data to server using fetch
-    fetch("/send-email", {
-        method: "POST",
-        body: formData
-    })
-    .then(response => response.json())
-    .then(data => {
-        document.querySelector(".alert").style.display = "block";
-        document.getElementById("messages").innerHTML = data.message;
-    })
-    .catch(error => {
-        console.error("Error:", error);
-        document.getElementById("messages").innerHTML = "An error occurred while submitting the form.";
-    });
+document.addEventListener("DOMContentLoaded", function() {
+    // Add event listener to the form when the DOM is fully loaded
+    const contactForm = document.getElementById("contactForm");
+    if (contactForm) {
+        contactForm.addEventListener("submit", function(event) {
+            event.preventDefault(); // Prevent default form submission
+            
+            // Collect form data
+            const formData = new FormData(this);
+            
+            // Send form data to server using fetch
+            fetch("/send-email", {
+                method: "POST",
+                body: formData
+            })
+            .then(response => response.json())
+            .then(data => {
+                document.querySelector(".alert").style.display = "block";
+                document.getElementById("messages").innerHTML = data.message;
+            })
+            .catch(error => {
+                console.error("Error:", error);
+                document.getElementById("messages").innerHTML = "An error occurred while submitting the form.";
+            });
+        });
+    } else {
+        console.error("Contact form element not found.");
+    }
 });
+
