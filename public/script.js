f954269e7ba763638063971377e8b2274c466ada
Iamouma <stephenouma452@gmail.com>
2024-08-03 17:36:07 +0300

    Minor changes

--- public/script.js
+++ public/script.js
@@ -47,3 +47,16 @@ document.addEventListener("DOMContentLoaded", function() {
         console.error("Contact form element not found.");
     }
 });
+
+function toggleTheme() {
+    const body = document.querySelector("body");
+    const icon = document.getElementById("icon");
+    if (body.className === 'dark') {
+        body.className = 'light'
+        icon.className = 'fa fa-moon-o'
+    }
+    else {
+        body.className = 'dark'
+        icon.className = 'fa fa-sun-o'
+    }
+}
